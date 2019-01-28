98464802c14b4467fa793cd260ea2c68b8369ef5
Daniel Nguy <daniel.nguy@stud.kftg.ch>
2019-01-28 13:58:13 +0100

    lebenslauf

---
+++ index.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Daniel Nguy</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/resume.min.css" rel="stylesheet">
+    <link href="css/resume.css" rel="stylesheet">
+    <link href="css/stylesheet.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-block d-lg-none">Daniel Nguy</span>
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/Daniel.jpg" alt="">
+        </span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#startseite">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#CV">CV</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#Contact">Contact</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#Weather">Weather</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#interests">Interests</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#awards">Awards</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container-fluid p-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex d-column" id="startseite">
+        <div class="my-auto">
+            <h1 class="mb-0">Daniel
+                <span class="text-primary">Nguy</span>
+            </h1>
+            <div class="subheading mb-5">9542 Münchwilen · Neuwies 4 · +41 76 403 62 89 ·
+                <a href="mailto:name@email.com">daniel.nguy@stud.kftg.ch</a>
+            </div>
+            <p class="lead mb-5">I am currently visiting the IT-College in Frauenfeld. As for now I am in the graduation
+                year and looking for an internship.</p>
+            <hr>
+            <div class="social-icons">
+                <a href="https://github.com/DanielNguy">
+                    <i class="fab fa-github"></i>
+                </a>
+                <a href="https://www.facebook.com/daniel.nguy.50">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+            </div>
+        </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="CV">
+        <div class="my-auto">
+            <h2 class="mb-5">CV - curriculum vitae</h2><a href="/download/Lebenslauf_Daniel.doc" download class="text-md-right">Download</a>
+            <h2 class="mb-5">Personal Information</h2>
+            <div class="text-md-left">
+                <p>Daniel Nguy
+                    <br>
+                    Neuwies 4
+                    <br>
+                    CH-9542 Münchwilen TG
+                    <br>
+                    +41 76 403 62 89
+                    <br>
+                    daniel.nguy@stud.kftg.ch
+                    <br>
+                    02.11.2000</p>
+            </div>
+            <h3 class="mb-0">Family</h3>
+
+            <div class="subheading mb-3">Tri Bill Nguy (Father 55y.)</div>
+            <p>Polytechnician, Sealed Air GmbH</p>
+            <div class="subheading mb-3">Thoai Lam Nguy (Mother 53y.)</div>
+            <p>Housewife</p>
+            <div class="subheading mb-3">Yin Nguy (Sister 30y.)</div>
+            <p>Specialist Providermanagement & IT Controlling, Swica</p>
+            <div class="subheading mb-3">Jackie Nguy (Brother 22y.)</div>
+            <p>Student in business computer science, ZHAW</p>
+            <div class="subheading mb-3">Ricky Nguy (Brother 20y.)</div>
+            <p>Polygraph in education, Maxsolution GmbH</p>
+
+<br>
+            <h2 class="mb-5">School</h2>
+            <div class="resume-item d-flex flex-column flex-md-row mb-5">
+                <div class="resume-content mr-auto">
+                    <h3 class="mb-0">Primary School</h3>
+                    <p>I visited the primary school Oberhofen in Münchwilen. During that time I have not known yet that
+                        I would be heading the way of an IT-Student. </p>
+                </div>
+                <div class="resume-date text-md-right">
+                    <span class="text-primary">2006 - 2013</span>
+                </div>
+            </div>
+
+            <div class="resume-item d-flex flex-column flex-md-row mb-5">
+                <div class="resume-content mr-auto">
+                    <h3 class="mb-0">Secondary School</h3>
+                    <p>The secondary school was in Münchwilen as well but not quite at the same place. The school is
+                        called secondary school Waldegg. As the time went by I have slowly figured out that my career
+                        would head towards a job in the IT.</p>
+                </div>
+                <div class="resume-date text-md-right">
+                    <span class="text-primary">2013 - 2016</span>
+                </div>
+            </div>
+
+            <div class="resume-item d-flex flex-column flex-md-row mb-5">
+                <div class="resume-content mr-auto">
+                    <h3 class="mb-0">College</h3>
+                    <p>After the secondary school it was clear for me to finish an education as an IT-Student. Therefore
+                        I have been visiting the IT-College of Frauenfeld since 2016. Now that I am in the graduation
+                        year I am looking for an internship to finish the education properly.</p>
+                </div>
+                <div class="resume-date text-md-right">
+                    <span class="text-primary">2016 - present</span>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="Contact">
+        <div class="my-auto">
+            <h2 class="mb-5">Contact</h2>
+
+            <form id="contact-form" method="post" action="index.php" role="form">
+                <div class="controls">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="form_name">Vorname</label>
+                                <input id="form_name" type="text" pattern="[a-zA-Z]*" name="vorname" class="form-control" placeholder="Vorname"
+                                       required="required" data-error="Der Vorname wird benötigt">
+                                <div class="help-block with-errors"></div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="form_lastname">Nachname</label>
+                                <input id="form_lastname" type="text" pattern="[a-zA-Z]*" name="nachname" class="form-control"
+                                       placeholder="Nachname" required="required" data-error="Der Nachname wird benötigt">
+                                <div class="help-block with-errors"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="form_email">E-Mail</label>
+                                <input id="form_email" type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" name="email" class="form-control"
+                                       placeholder="E-Mail" required="required" data-error="Die E-Mail wird benötigt">
+                                <div class="help-block with-errors"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="form_message">Nachricht</label>
+                                <textarea id="form_message" name="nachricht" class="form-control" placeholder="Nachricht"
+                                          rows="4" maxlength="255" required="required" data-error="Geben Sie eine Nachricht ein"></textarea>
+                                <div class="help-block with-errors"></div>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <input type="submit" class="btn btn-success btn-send btn-bottom" value="Abschicken" name="Abschicken" id="Abschicken">
+                        </div>
+                    </div>
+                </div>
+            </form>
+            <div id="sendsuccess" class="col-md-12" align="center" style="display: none;">
+                <p>Vielen Dank für Ihre Nachricht!</p>
+            </div>
+        </div>
+        </main>
+
+    </section>
+        <?php
+        function sendmail() {
+
+            $empfaenger = "daniel.nguy@stud.kftg.ch";
+            $nachname = $_POST['nachname'];
+            $vorname = $_POST['vorname'];
+            $from = $_POST['email'];
+            $text = $_POST['nachricht'];
+            $msg = wordwrap($text, 70);
+            mail($empfaenger, $msg, $from);
+
+
+        }
+        function sendenerfolgreich() {
+            exit();
+        }
+        if (array_key_exists('Abschicken', $_POST)) {
+            sendmail();
+            echo '<script type="text/javascript">';
+            echo 'document.getElementById("sendsuccess").style.display = "block";';
+            echo '</script>';
+        }
+        ?>
+
+        </div>
+
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="Weather">
+        <div class="my-auto">
+            <h2 class="mb-5">Weather</h2>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
+        <div class="my-auto">
+            <h2 class="mb-5">Interests</h2>
+            <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid
+                skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free
+                climbing, and kayaking.</p>
+            <p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television
+                shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology
+                advancements in the front-end web development world.</p>
+        </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="awards">
+        <div class="my-auto">
+            <h2 class="mb-5">Awards &amp; Certifications</h2>
+            <ul class="fa-ul mb-0">
+                <li>
+                    <i class="fa-li fa fa-trophy text-warning"></i>
+                    Google Analytics Certified Developer
+                </li>
+                <li>
+                    <i class="fa-li fa fa-trophy text-warning"></i>
+                    Mobile Web Specialist - Google Certification
+                </li>
+                <li>
+                    <i class="fa-li fa fa-trophy text-warning"></i>
+                    1<sup>st</sup>
+                    Place - University of Colorado Boulder - Emerging Tech Competition 2009
+                </li>
+                <li>
+                    <i class="fa-li fa fa-trophy text-warning"></i>
+                    1<sup>st</sup>
+                    Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)
+                </li>
+                <li>
+                    <i class="fa-li fa fa-trophy text-warning"></i>
+                    2<sup>nd</sup>
+                    Place - University of Colorado Boulder - Emerging Tech Competition 2008
+                </li>
+                <li>
+                    <i class="fa-li fa fa-trophy text-warning"></i>
+                    1<sup>st</sup>
+                    Place - James Buchanan High School - Hackathon 2006
+                </li>
+                <li>
+                    <i class="fa-li fa fa-trophy text-warning"></i>
+                    3<sup>rd</sup>
+                    Place - James Buchanan High School - Hackathon 2005
+                </li>
+            </ul>
+        </div>
+    </section>
+
+</div>
+
+<!-- Bootstrap core JavaScript -->
+
+<script src="geolocation.js"></script>
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for this template -->
+<script src="js/resume.min.js"></script>
+
+</body>
+
+</html>
